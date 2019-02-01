dacb377261871c53949712f39390decc2f9a5be6
ljh <943497785@qq.com>
2019-02-01 16:10:21 +0800

    init

---
+++ updatePip/__init__.py
@@ -0,0 +1,7 @@
+from pip._internal.utils.misc import get_installed_distributions
+from subprocess import call
+
+for dist in get_installed_distributions():
+    print(dist)
+    print(dist.project_name)
+    call("pip3 install --upgrade "+dist.project_name,shell=True)
